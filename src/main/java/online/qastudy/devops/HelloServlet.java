05258d8a13ea11d32cfc65944e23f795d1fc1d23
Evhenii <78321817+YevheniiHryshkovets@users.noreply.github.com>
2024-01-08 00:16:16 +0200

    Update HelloServlet.java

--- src/main/java/online/qastudy/devops/HelloServlet.java
+++ src/main/java/online/qastudy/devops/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World! UPDATED MESSAGE v14";
+        message = "Hello World! UPDATED MESSAGE v16";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
